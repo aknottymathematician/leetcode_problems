ddd269f288c30064c5887e20066c6724795a2e97
aknottymathematician <69105395+aknottymathematician@users.noreply.github.com>
2022-03-03 11:37:56 +0530

    Time: 692 ms (72.40%), Space: 27.3 MB (75.51%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,15 +1,27 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        mSum = 0
-        mCount = 0
-        mStart = 0
+#         mSum = 0
+#         mCount = 0
+#         mStart = 0
 
-        for mEnd in range(len(arr)):
-            mSum += arr[mEnd]
-            if mEnd >= k:
-                mSum -= arr[mStart]
-                mStart +=1
-            if (mSum//k) >= threshold and (mEnd-mStart+1)==k:
-                mCount += 1
+#         for mEnd in range(len(arr)):
+#             mSum += arr[mEnd]
+#             if mEnd >= k:
+#                 mSum -= arr[mStart]
+#                 mStart +=1
+#             if (mSum//k) >= threshold and (mEnd-mStart+1)==k:
+#                 mCount += 1
 
-        return mCount
+#         return mCount
+
+        wstart = wsum = count = 0
+        
+        for wend in range(len(arr)):
+            wsum += arr[wend]
+            
+            if wend >= k:
+                wsum -= arr[wstart]
+                wstart += 1
+            if (wsum//k) >= threshold and (wend-wstart+1) == k: 
+                count += 1
+        return count
