f3d0dc2db528af5f069dee62b59131c0be56d59a
aknottymathematician <69105395+aknottymathematician@users.noreply.github.com>
2022-04-07 21:20:48 +0530

    Time: 40 ms (65.73%), Space: 13.8 MB (97.50%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # first, negate all weight values in-place
+        for i, s in enumerate(stones):
+            stones[i] = -s
+        heapify(stones)  # pass all negated values into the min-heap
+        while stones:
+            s1 = -heappop(stones)  # the heaviest stone
+            if not stones:  # s1 is the remaining stone
+                return s1
+            s2 = -heappop(stones)  # the second-heaviest stone; s2 <= s1
+            if s1 > s2:
+                heappush(stones, s2-s1)  # push the NEGATED value of s1-s2; i.e., s2-s1
+            # else s1 == s2; both stones are destroyed
+        return 0  # if no more stones remain
